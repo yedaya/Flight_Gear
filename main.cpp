695979d8cf60853f762db7f9b19ec100fbeecf96
yedaya <45755614+yedaya@users.noreply.github.com>
2018-12-26 21:06:46 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <cstring>
+#include <list>
+#include <fstream>
+#include <sstream>
+#include "Interpreter.h"
+#include "ReadData.h"
 
-int main() {
-    unsigned x = 0xffffffff;
-    std::cout << x  << std::endl;
+using namespace std;
+
+
+int main(int argc, char** argv) {
+    try {
+        ReadData readCode;
+
+        // if there is arg, read from file
+        if (argc > 1) {
+            readCode.readFromFile(argv[1]);
+            // else, read from console
+        } else {
+            readCode.readFromConsole();
+        }
+        cout << "\nCya next time :)\n" << endl;
+
+    } catch (...) {
+        cout << "\nCareful!!\n" << endl;
+    }
+
     return 0;
 }
